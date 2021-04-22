7872e57a244780fc3ab74f09527cd7219932e180
ATimpe <atimpe@csumb.edu>
2021-04-22 12:37:04 -0700

    Added Landing Activity

--- app/src/main/java/com/example/duofinder/LandingActivity.java
+++ app/src/main/java/com/example/duofinder/LandingActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class LandingActivity extends AppCompatActivity {
     private Button mProfileBtn;
