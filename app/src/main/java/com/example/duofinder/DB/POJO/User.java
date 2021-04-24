e70451c95da50bf1e61461e1b59af680a2f20d98
Daniel Rangel <drangelfigueroa@csumb.edu>
2021-04-23 17:11:45 -0700

    User can edit their games list

---
+++ app/src/main/java/com/example/duofinder/DB/POJO/User.java
@@ -0,0 +1,31 @@
+package com.example.duofinder.DB.POJO;
+
+import java.util.Objects;
+
+public class User {
+    public String username;
+    public String email;
+
+    public User() {
+
+    }
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username) &&
+                Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, email);
+    }
+}
