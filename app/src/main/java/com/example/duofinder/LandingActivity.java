e123ecd696aa519b67165e289d9612eb503105e2
Daniel Rangel <drangelfigueroa@csumb.edu>
2021-04-24 14:44:25 -0700

    Implemented logout. Wired up LandingActivity

--- app/src/main/java/com/example/duofinder/LandingActivity.java
+++ app/src/main/java/com/example/duofinder/LandingActivity.java
@@ -7,40 +7,67 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LandingActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private TextView mUsernameTV;
+    private TextView mLogoutTV;
+    private Button mChatBtn;
     private Button mProfileBtn;
-    private Button mLogoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+        wireUp();
 
-        mProfileBtn = findViewById(R.id.buttonProfile);
-        mLogoutBtn = findViewById(R.id.buttonLogout);
-
-        mProfileBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = ProfileActivity.intentFactory(getApplicationContext());
-                finish();
-                startActivity(intent);
-            }
-        });
-
-        mLogoutBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = SignUpActivity.intentFactory(getApplicationContext());
-                finish();
-                startActivity(intent);
-            }
-        });
     }
 
+    public void wireUp() {
+        mAuth = FirebaseAuth.getInstance();
+        mUsernameTV = findViewById(R.id.textViewUsername);
+        mUsernameTV.setText(MainActivity.USER.username);
+
+        mLogoutTV = findViewById(R.id.textViewLogout);
+        mLogoutTV.setOnClickListener(v -> logout());
+
+        mChatBtn = findViewById(R.id.buttonChat);
+        mChatBtn.setOnClickListener(v -> {
+            //todo: ChatActivity
+        });
+
+        mProfileBtn = findViewById(R.id.buttonProfile);
+        mProfileBtn.setOnClickListener(v -> {
+            startActivity(ProfileActivity.intentFactory(this));
+        });
+
+        if (MainActivity.USER.isAdmin) {
+            Button mAdminBtn = findViewById(R.id.buttonAdmin);
+            mAdminBtn.setVisibility(View.VISIBLE);
+            mAdminBtn.setOnClickListener(v -> {
+                //todo: AdminActivity
+            });
+        }
+
+    }
+
+    /**
+     * Factory pattern provided Intent to switch to this activity.
+     *
+     * @param ctx the Context to switch from
+     * @return the Intent to switch to this activity
+     */
     public static Intent intentFactory(Context ctx) {
         Intent intent = new Intent(ctx, LandingActivity.class);
         return intent;
+    }
+
+    public void logout() {
+        mAuth.signOut();
+        startActivity(MainActivity.intentFactory(LandingActivity.this));
+        finish();
     }
 }
