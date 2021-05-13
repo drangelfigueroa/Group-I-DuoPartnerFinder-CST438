45146d257eb65984eb34fa5bdccdad69b00b42ac
Daniel Rangel Figueroa <drangelfigueroa@csumb.edu>
2021-05-13 00:55:20 -0700

    Added tests.

--- app/src/androidTest/java/com/example/duofinder/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/duofinder/ExampleInstrumentedTest.java
@@ -1,12 +1,22 @@
 package com.example.duofinder;
 
 import android.content.Context;
-
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.Objects;
 
 import static org.junit.Assert.*;
 
@@ -17,10 +27,138 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    private final DatabaseReference mRef = FirebaseDatabase.getInstance().getReference();
+    private final String uid = "ZEnyGPR8iHYQHAwH3z2Cf2ZTOvN2";
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.duofinder", appContext.getPackageName());
+    }
+
+    @Test
+    public void pullUserTestAssertFail(){
+        mRef.child("Users").child("0").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @org.jetbrains.annotations.NotNull DataSnapshot snapshot) {
+                if(snapshot.exists())
+                    fail();
+            }
+
+            @Override
+            public void onCancelled(@NonNull @org.jetbrains.annotations.NotNull DatabaseError error) {
+            }
+        });
+    }
+
+    @Test
+    public void pullUserTestAssertPass(){
+        mRef.child("Users").child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @org.jetbrains.annotations.NotNull DataSnapshot snapshot) {
+                if(!snapshot.exists())
+                    fail();
+            }
+
+            @Override
+            public void onCancelled(@NonNull @org.jetbrains.annotations.NotNull DatabaseError error) {
+                fail();
+            }
+        });
+    }
+
+    @Test
+    public void pullConversationAssertFail(){
+        mRef.child("Conversations").child("0").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @org.jetbrains.annotations.NotNull DataSnapshot snapshot) {
+                if(snapshot.exists())
+                    fail();
+            }
+
+            @Override
+            public void onCancelled(@NonNull @org.jetbrains.annotations.NotNull DatabaseError error) {
+            }
+        });
+    }
+
+    @Test
+    public void pullConversationAssertPass(){
+        mRef.child("Conversations").child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @org.jetbrains.annotations.NotNull DataSnapshot snapshot) {
+                if(!snapshot.exists())
+                    fail();
+            }
+
+            @Override
+            public void onCancelled(@NonNull @org.jetbrains.annotations.NotNull DatabaseError error) {
+                fail();
+            }
+        });
+    }
+
+    @Test
+    public void pullGameAssertFail(){
+        mRef.child("Game").child("0").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @org.jetbrains.annotations.NotNull DataSnapshot snapshot) {
+                if(snapshot.exists())
+                    fail();
+            }
+
+            @Override
+            public void onCancelled(@NonNull @org.jetbrains.annotations.NotNull DatabaseError error) {
+            }
+        });
+    }
+
+    @Test
+    public void pullGameAssertPass(){
+        mRef.child("Game").child("ACNH").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @org.jetbrains.annotations.NotNull DataSnapshot snapshot) {
+                if(!snapshot.exists())
+                    fail();
+            }
+
+            @Override
+            public void onCancelled(@NonNull @org.jetbrains.annotations.NotNull DatabaseError error) {
+                fail();
+            }
+        });
+    }
+
+    @Test
+    public void pullPlaysAssertFail(){
+        mRef.child("Plays").child("0").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @org.jetbrains.annotations.NotNull DataSnapshot snapshot) {
+                if(snapshot.exists())
+                    fail();
+            }
+
+            @Override
+            public void onCancelled(@NonNull @org.jetbrains.annotations.NotNull DatabaseError error) {
+            }
+        });
+    }
+
+    @Test
+    public void pullPlaysAssertPass(){
+        String playsId = "-M_ZfTV0zWk58-O353bU";
+        mRef.child("Plays").child(playsId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @org.jetbrains.annotations.NotNull DataSnapshot snapshot) {
+                if(!snapshot.exists())
+                    fail();
+            }
+
+            @Override
+            public void onCancelled(@NonNull @org.jetbrains.annotations.NotNull DatabaseError error) {
+                fail();
+            }
+        });
     }
 }
